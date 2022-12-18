03eda55ae1a773935c91261fb3b354c2594ee434
dustjq1004 <dustjq1005@naver.com>
2022-12-18 21:31:48 +0900

    javscript/es6 자바스크립트에 대해 알아보자 - 자바스크립트의 배열01

---
+++ es6/1_study/2_array/2_array_01.js
@@ -0,0 +1,18 @@
+var numArr = [1, 2, 3, 4, 5, 6];
+var stringArr = ["똘기", "떵이", "호치", "세쵸미"];
+var constructorArr = new Array(1,2,3,4,"5");
+
+
+console.log(numArr[0]);
+console.log(numArr[1]);
+console.log(numArr[2]);
+console.log(numArr[3]);
+
+console.log(stringArr);
+console.log(constructorArr[0]);
+console.log(constructorArr[4]);
+
+
+module.exports = {
+    numArr,stringArr,constructorArr
+};
---
+++ es6/2_test/2_array_test_01/2_array_test_01.test.js
@@ -0,0 +1,12 @@
+const array01 = require("../../1_study/2_array/2_array_01");
+
+test("배열에 대해서 알아보자", () =>{
+    array01.numArr.forEach((value, index)=> {
+        expect(value).toBe(index + 1);
+        console.log(`numArr[${index}] : ${value}`);
+    });
+    array01.constructorArr.forEach((value, index)=> {
+        console.log(`consructorArr[${index}] : ${value}`);
+        expect(typeof value).toEqual("number");
+    });
+});
